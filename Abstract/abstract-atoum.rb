ad132073c56897bea3b4aa7d07d5a28509f66209
jubianchi <contact@jubianchi.fr>
2017-02-15 00:53:08 +0100

    Use own abstract formula

--- Abstract/abstract-atoum.rb
+++ Abstract/abstract-atoum.rb
@@ -1,18 +1,30 @@
-require File.expand_path("../../homebrew-php/Abstract/abstract-php-phar", __FILE__)
-
-class AbstractAtoum < AbstractPhpPhar
+class AbstractAtoum < Formula
   init
   desc "The modern, simple and intuitive PHP unit testing framework."
   homepage "http://atoum.org/"
 
-  def phar_file
+  def phar
     class_name = self.class.name.split("::").last.split("AT").first
     class_name.downcase + ".phar"
   end
 
-  def phar_bin
+  def stub
     class_name = self.class.name.split("::").last.split("AT").first
     class_name.downcase
+  end
+
+  def wrapper
+    <<-EOS.undent
+      #!/usr/bin/env bash
+      /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/#{@phar} "$@"
+    EOS
+  end
+
+  def install
+    libexec.install phar
+    (libexec/stub).write wrapper
+    chmod 0755, (libexec/stub)
+    bin.install_symlink (libexec/stub)
   end
 
   test do
