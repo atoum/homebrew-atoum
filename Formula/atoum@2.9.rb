0b1f0bd31ac7af5b1174760666dcb2b87a8e673f
jubianchi <contact@jubianchi.fr>
2017-02-24 14:22:43 +0100

    Typo

--- Formula/atoum@2.9.rb
+++ Formula/atoum@2.9.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract-atoum-phar", __FILE__)
 
-class Atoum < AbstractAtoumPhar
+class AtoumAT29 < AbstractAtoumPhar
   url "https://github.com/atoum/atoum/releases/download/2.9.0/atoum.phar"
   sha256 "5e36d71bdff57c16dd55eaf948671ac72d561f32bd4bca32b5891d02bdca43b8"
 
